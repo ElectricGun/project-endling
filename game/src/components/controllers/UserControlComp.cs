84bd0556ad5e7be73548ed0d74245ba53a958f12
ElectricGun <->
2024-11-10 04:54:23 +0700

    removed unnecessary import

--- game/src/components/controllers/UserControlComp.cs
+++ game/src/components/controllers/UserControlComp.cs
@@ -1,15 +1,12 @@
 using Godot;
 using System;
-using System.Diagnostics.Contracts;
 
 [GlobalClass]
 public partial class UserControlComp : BaseAIComp
 {
-
     public override void _Process(double delta)
     {
         MoveDirX = (Input.IsActionPressed(InputNames.left) ? -1 : 0) +  (Input.IsActionPressed(InputNames.right) ? 1 : 0);
 		IsJumping = (Input.IsActionPressed(InputNames.jump));
     }
-
 }
